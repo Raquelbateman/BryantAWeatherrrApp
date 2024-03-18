9c3441f21a8e545dc9baf8ce4273fe7d29bb966d
Raquelbateman <aliciaraquel1017@gmail.com>
2024-03-17 19:26:33 -0700

    small strides

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,5 @@ let dayAndDate = document.getElementById("dayAndDate");
 let currentLocation = document.getElementById("currentLocation");
 let dayOfWeek = document.getElementById("dayOfWeek");
 let dayOfWeekTemp = document.getElementById("dayOfWeekTemp");
+let weatherIcon = document.getElementById("weatherIcon");
+
